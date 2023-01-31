0bfbbb8af7d4f519c093cebf92868b4a3af0edf0
JesusRH97 <jrh.hdlt@gmail.com>
2023-01-31 20:52:57 +0100

    Ploted distance beetwen fingers and FPS

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -9,7 +9,7 @@ wCam, hCam = 640, 480
 pTime = 0
 cTime = 0
 cont = 0
-average = np.zeros((10, ))
+values = np.zeros((10, ))
 
 cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
@@ -18,38 +18,42 @@ cap.set(4, hCam)
 detector = htm.handDetector()
 
 while True:
-    success, img = cap.read()
-    img = cv2.resize(img, (0,0), fx=1.5, fy=1.5)
+    for i in range(len(values)):
 
-    img = detector.findHands(img, draw=False)
-    lmList = detector.findPosition(img, draw=False)
+        success, img = cap.read()
+        img = cv2.resize(img, (0,0), fx=1.5, fy=1.5)
 
-    cTime = time.time()
-    #print(cTime - pTime)
-    pTime = cTime
+        img = detector.findHands(img, draw=False)
+        lmList = detector.findPosition(img, draw=False)
 
-    if len(lmList):
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
 
-        #Big finger
-        cv2.circle(img, (lmList[4][1], lmList[4][2]), 15, (255, 0, 255), cv2.FILLED)
-        big_finger = math.hypot(lmList[4][1] - lmList[0][1], lmList[4][2] - lmList[0][2])
+        cv2.putText(img, str(int(fps))+" FPS", (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                    (255, 0, 255), 3)
 
-        #Index finger
-        cv2.circle(img, (lmList[8][1], lmList[8][2]), 15, (255, 0, 255), cv2.FILLED)
-        index_finger = math.hypot(lmList[8][1] - lmList[0][1], lmList[8][2] - lmList[0][2])
+        if len(lmList):
 
-        #Line between both fingers
-        cv2.line(img, (lmList[4][1], lmList[4][2]), (lmList[8][1], lmList[8][2]), (255, 0, 255), 3)
+            #Big finger
+            cv2.circle(img, (lmList[4][1], lmList[4][2]), 15, (255, 0, 255), cv2.FILLED)
+            big_finger = math.hypot(lmList[4][1] - lmList[0][1], lmList[4][2] - lmList[0][2])
 
-        distance_between_fingers = (math.hypot(lmList[4][1] - lmList[8][1], (lmList[4][2] - lmList[8][2]))) / (index_finger/17.0)
+            #Index finger
+            cv2.circle(img, (lmList[8][1], lmList[8][2]), 15, (255, 0, 255), cv2.FILLED)
+            index_finger = math.hypot(lmList[8][1] - lmList[0][1], lmList[8][2] - lmList[0][2])
 
-        average[cont] = distance_between_fingers
-        cont += 1
+            #Line between both fingers
+            cv2.line(img, (lmList[4][1], lmList[4][2]), (lmList[8][1], lmList[8][2]), (255, 0, 255), 3)
 
-        if cont == 10:
-            print(np.mean(average))
-            cont = 0
+            distance_between_fingers = (math.hypot(lmList[4][1] - lmList[8][1], (lmList[4][2] - lmList[8][2]))) / (index_finger/17.0)
 
+            values[i] = distance_between_fingers
 
-    cv2.imshow("Img", img)
-    cv2.waitKey(1)
+            #print(average[i])
+
+        cv2.imshow("Img", img)
+        cv2.waitKey(1)
+
+    print(np.mean(values))
+
